8f83da615a33cead5338954aadad4517e4db906c
Joe Heth <joeheth@gmail.com>
2012-09-15 19:21:47 -0400

    Adding more examples and primitive guess implementation.

--- code-breaker/lib/codebreaker/game.rb
+++ code-breaker/lib/codebreaker/game.rb
@@ -5,12 +5,21 @@ module Codebreaker
         end
 
         def start(secret)
+            @secret = secret
             @output.puts 'Welcome to Codebreaker!'
             @output.puts 'Enter Guess:'
         end
 
         def guess(guess)
-            @output.puts ''
+            mark = ''
+            if guess[0] == @secret[0]
+                mark = '+'
+            elsif @secret.include?(guess[0])
+                mark = '-'
+            else
+                mark = ''
+            end
+            @output.puts mark
         end
     end
 end
