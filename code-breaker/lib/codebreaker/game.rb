128303301aee5ce72202e12b950b927e41865441
Joe Heth <joeheth@gmail.com>
2012-09-15 20:29:03 -0400

    Adding example for new requirement and refactoring marker implementation to support it.

--- code-breaker/lib/codebreaker/game.rb
+++ code-breaker/lib/codebreaker/game.rb
@@ -24,8 +24,13 @@ module Codebreaker
         end
 
         def number_match_count
-            (0..3).inject(0) do |count, index|
-                count + (number_match?(index) ? 1 : 0)
+            total_match_count - exact_match_count
+        end
+
+        def total_match_count
+            secret = @secret.split('')
+            @guess.split('').inject(0) do |count, n|
+                count + (delete_first(secret, n) ? 1 : 0)
             end
         end
 
@@ -33,6 +38,10 @@ module Codebreaker
             (0..3).inject(0) do |count, index|
                 count + (exact_match?(index) ? 1 : 0)
             end
+        end
+
+        def delete_first(code, n)
+            code.delete_at(code.index(n)) if code.include?(n)
         end
 
         def exact_match?(index)
